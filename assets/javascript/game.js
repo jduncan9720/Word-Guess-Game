dcecb0b57a8999c2af449bf48b5e7ddca6d6dfe8
Joshua Duncan <jduncan97@gmail.com>
2020-03-10 14:52:58 -0600

    Added the JS to make the wrong letters display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,47 @@
-document.addEventListener('keydown', newWord);
-
-function newWord()
+var newSetup = "true";
+var word = "NULL";
+var answerArray = [];
+var wrongArray = [];
+document.onkeyup = function newWord(event)
 {
-    var words = [
-        "horse",
-        "falcon",
-        "monkey",
-        "goose",
-        "hamster",
-        "tortoise",
-    ];
-    
-    var word = words[Math.floor(Math.random() * words.length)];
-    
-    var answerArray = [];
-    for (var i = 0; i < word.length; i++) {
-        answerArray[i] = "_";
-    }
-    
-    var result = answerArray.join(' ');
+    var letter = event.key.toLowerCase();
+   if(newSetup == "false"){
+       wordCheck(letter);
+   } 
+   else{
+        var words = [
+            "horse"
+            // "falcon",
+            // "monkey",
+            // "goose",
+            // "hamster",
+            // "tortoise"
+        ];
+        
+        word = words[Math.floor(Math.random() * words.length)];
+        
+        for (var i = 0; i < word.length; i++) {
+            answerArray[i] = "_";
+        }
+        
+        var result = answerArray.join(' ');
 
-    document.getElementById('currentWord').innerHTML = result;
+        document.getElementById('currentWord').innerHTML = result;
+        newSetup = "false";
+    }
 }
 
-
+function wordCheck(letter) {
+    if (word.includes(letter)) {
+        answerArray[word.indexOf(letter)] = letter;
+        var result = answerArray.join(' ');
+        document.getElementById('currentWord').innerHTML = result;
+    }
+    else{
+        if(!wrongArray.includes(letter)){
+            wrongArray.push(letter);
+            document.getElementById('lettersGuessed').innerHTML = wrongArray;
+        }
+    }
+}
 
