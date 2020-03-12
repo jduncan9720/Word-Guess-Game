eb0cd7cc32e473a9521ab50ab0464f2eb3ae4745
Joshua Duncan <jduncan97@gmail.com>
2020-03-12 17:20:39 -0600

    Added more JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -57,19 +57,26 @@ document.onkeyup = function newWord(event)
 
 //Checks the letter and updates if it is in the given word.
 function wordCheck(letter) {
+    //Check if the word contains any instance of the letter
     if (word.includes(letter)) {
+        //Find the first instance of the letter occuring in the word
         var position = word.indexOf(letter);
+
+        //While there are instances of the letter remaining, update the display
         while (position != -1){
             answerArray[position] = letter;
             var result = answerArray.join(' ');
             document.getElementById('currentWord').innerHTML = result;
             position = word.indexOf(letter, position + 1);
         }
+
+        //If all the _ have been replaced, win the game.
         if (!answerArray.includes("_")){
             winGame();
         }
     }
     else{
+        //We know the letter is not in the word, check if it's been processed before
         if(!wrongArray.includes(letter)){
             wrongArray.push(letter);
             document.getElementById('lettersGuessed').innerHTML = wrongArray;
@@ -78,6 +85,7 @@ function wordCheck(letter) {
     }
 }
 
+//Lose a single life, if you are at zero, lose the game
 function loseLife() {
     lives--;
     document.getElementById('guessesRemain').innerHTML = lives;
@@ -87,15 +95,21 @@ function loseLife() {
     }
 }
 
+//Win the game and show a picture
 function winGame() {
     document.getElementById('currentWord').innerHTML = "YOU WIN!..PLAY AGAIN";
     wins++;
     document.getElementById('winsTotal').innerHTML = wins;
     newSetup = "true";
+    
+    //Find the associated picture with the win.
     if (word == "goose") {
         document.getElementById("image").src = "assets/images/goose.jpg";
     }
-    else if(word == "ethan"){
-
+    else if(word == "horse"){
+        document.getElementById("image").src = "assets/images/goose.jpg";
+    }
+    else if(word == "falcon"){
+        document.getElementById("image").src = "assets/images/goose.jpg";
     }
 }
